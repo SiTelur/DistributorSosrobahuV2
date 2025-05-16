6f0ae1772c5c2ae2c107634b011a9aaed1a82a98
DimasAswito <dimasaswito@gmail.com>
2025-05-16 16:07:36 +0700

    Create cetak-restock.blade.php

---
+++ resources/views/pabrik/cetak-restock.blade.php
@@ -0,0 +1,50 @@
+<div id="pdf-content" class="rounded-lg shadow-lg px-8 py-6 max-w-3xl mx-auto">
+        <div class="flex items-center justify-between mb-6">
+            <div class="flex items-center">
+                <img class="h-20 w-30 mr-4" src="{{ asset('assets/images/logo.png') }}" alt="Logo" />
+            </div>
+            <div class="text-gray-700 text-right">
+                <div class="font-bold text-2xl mb-1">RESTOCK PRODUK</div>
+                <div class="text-sm">{{ $notaPabrik['tanggal'] }}</div>
+                <div class="text-sm">RST1234{{$notaPabrik['id_restock']}}</div>
+                <div class="text-sm font-semibold">Official CV. Santoso Jaya Tembakau</div>
+            </div>
+        </div>
+        <table class="w-full border-separate border-spacing-0 mb-6 rounded-lg">
+            <thead class="bg-gray-800 text-gray-300">
+                <tr>
+                    <th class="p-3 text-center">Nama Produk</th>
+                    <th class="p-3 text-center">Jumlah (Karton)</th>
+                </tr>
+            </thead>
+            <tbody>
+                @php
+                    $totalJumlah = 0; // Inisialisasi total jumlah
+                @endphp
+                @if(!empty($notaPabrik['item_nota']))
+                    @foreach ($notaPabrik['item_nota'] as $item)
+                        <tr class="border-b">
+                            <td class="p-3 text-center">{{ $item['nama_rokok'] }}</td>
+                            <td class="p-4 text-center">{{ $item['jumlah_item'] }}</td>
+                        </tr>
+                        @php
+                            $totalJumlah += $item['jumlah_item']; // Tambahkan jumlah ke total
+                        @endphp
+                    @endforeach
+                @else
+                    <tr>
+                        <td colspan="2" class="text-center">Data tidak tersedia</td>
+                    </tr>
+                @endif
+                <!-- Baris untuk total keseluruhan -->
+                <tr class="bg-white text-lg font-semibold">
+                    <td class="p-3 text-right">Total Keseluruhan</td>
+                    <td class="p-3 text-center" id="total-jumlah">{{ $totalJumlah }} Karton</td>
+                </tr>
+            </tbody>
+        </table>
+        <div class="border-t-2 border-gray-300 pt-8">
+            <p class="text-gray-600 mb-2">Detail riwayat restock produk ini adalah dokumen yang dicetak secara otomatis
+                oleh sistem berdasarkan data yang diinput oleh perusahaan.</p>
+        </div>
+    </div>
