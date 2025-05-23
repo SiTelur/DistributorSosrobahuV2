4ca2a511abb1c36e4d712751bb762f8673b64b69
SiTelur <gantengrizky440@gmail.com>
2025-05-23 07:49:46 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+
+    server: {
+        host: "https://internal-antelope-saved.ngrok-free.app"
+    },
+
+
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
